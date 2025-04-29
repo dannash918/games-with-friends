f581b2f17239d0a6ea23dd604e8bd283a4e9c6d0
dannash918 <dannash918@gmail.com>
2025-04-29 15:06:13 +1000

    fix linting

--- src/app/games/music-quiz/_party/questions.ts
+++ src/app/games/music-quiz/_party/questions.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/require-await */
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
